c5c87baef040a749cb009984543ffc8767da1b18
jochen.schmidt <jschmidt@cbs.mpg.de>
2023-02-14 11:55:50 +0100

    add error catches for number of samples; adding doc

--- rfpf/class.py
+++ rfpf/class.py
@@ -10,34 +10,6 @@ import pickle
 logModule = logging.getLogger(__name__)
 
 
-def load_external_rf(rf_file) -> np.ndarray:
-    """
-        if pulse profile is provided, read in
-        :param filename: name of file (txt or pta) of pulse
-        :return: pulse array, pulse length
-        """
-    # load file content
-    ext_file = plib.Path(rf_file)
-    with open(ext_file, "r") as f:
-        content = f.readlines()
-
-    # find line where actual data starts
-    start_count = -1
-    while True:
-        start_count += 1
-        line = content[start_count]
-        start_line = line.strip().split('\t')[0]
-        if start_line.replace('.', '', 1).isdigit():
-            break
-
-    # read to array
-    content = content[start_count:]
-    temp = [line.strip().split('\t')[0] for line in content]
-
-    pulseShape = np.array(temp, dtype=float)
-    return pulseShape
-
-
 @dc.dataclass
 class ExtRf:
     filename: str = ""
@@ -62,18 +34,6 @@ class ExtRf:
             self.phase
         )
 
-    def _att_to_dict(self):
-        atts = inspect.getmembers(self)
-        d = {}
-        for a in atts:
-            if not inspect.isroutine(a[1]):
-                if not a[0].startswith('__') and not a[0].endswith('__') and not a[0].startswith('_'):
-                    val = a[1]
-                    if isinstance(val, np.ndarray):
-                        val = a[1].tolist()
-                    d.__setitem__(a[0], val)
-        return d
-
     def save(self, f_name: typing.Union[str, plib.Path]):
         p_name = plib.Path(f_name).absolute()
         if p_name.parent.is_dir():
@@ -94,6 +54,15 @@ class ExtRf:
                       duration_in_us: int = None,
                       time_bandwidth: float = None,
                       num_samples: int = None):
+        """
+        read file from .txt or .pta. Need to fill the additional specs.
+        :param f_name: file name
+        :param bandwidth_in_Hz: Bandwidth in Hertz (optional if duration and tbw provided)
+        :param duration_in_us:  Duration in microseconds (optional if bandwidth and tbw provided)
+        :param time_bandwidth: Time Bandwidth product unitless (optional if bandwidth and duration provided)
+        :param num_samples: number of samples of pulse optional, if None pulse sampled per microsecond
+        :return:
+        """
         if bandwidth_in_Hz is None:
             if duration_in_us is None:
                 err = f"No bandwidth provided: provide Duration and time-bandwidth product"
@@ -129,13 +98,27 @@ class ExtRf:
                     break
 
             # read to array
+            if content.__len__() != num_samples:
+                logModule.info(f"file content not matching number of samples given {num_samples}")
+                num_samples = content.__len__() - start_count
+                logModule.info(f"adjusting number of samples to {num_samples}")
             content = content[start_count:start_count+num_samples]
             temp_amp = np.array([line.strip().split('\t')[0] for line in content])
             temp_phase = np.array([line.strip().split('\t')[1] for line in content])
 
             rf_cls.amplitude = temp_amp
             rf_cls.phase = temp_phase
+        if rf_cls.amplitude.shape[0] != rf_cls.phase.shape[0]:
+            err = "shape of amplitude and phase do not match"
+            logModule.error(err)
+            raise AttributeError(err)
         return rf_cls
+
+    def set_bandwidth_Hz(self, bw: float):
+        self.bandwidth_in_Hz = bw
+
+    def set_duration_us(self, duration: int):
+        self.duration_in_us = duration
 
 
 if __name__ == '__main__':
